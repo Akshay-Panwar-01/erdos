0c2660289ea63824cc29caefba0c941ca9a75c04
Akshay-Panwar-01 <65433527+Akshay-Panwar-01@users.noreply.github.com>
2023-09-20 21:25:40 +0530

    Create 231.py

---
+++ 231.py
@@ -0,0 +1,6 @@
+'''No doubt one of the easiest problem I ever encountered on erdos 
+we can remove at most (n//2)-1 elements from the set still there will be 
+one pair remaining.
+so the answer is 2019//2-1= 1008'''
+
+print(2019//2-1)

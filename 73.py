0f6c32cfbead079846569a618b7f26581299059c
Akshay-Panwar-01 <65433527+Akshay-Panwar-01@users.noreply.github.com>
2023-10-12 03:01:59 +0530

    Create 73.py

---
+++ 73.py
@@ -0,0 +1,8 @@
+'''Basically the problem has no integer solutions https://artofproblemsolving.com/wiki/index.php/2005_USAMO_Problems/Problem_2
+and so the answer is '''
+
+import sympy 
+import math 
+
+print(int(math.e**134))
+'''The answer is : 156841'''

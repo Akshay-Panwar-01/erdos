fb5e03635890a8594ed3b4b4ad47eef438bfb523
Akshay-Panwar-01 <65433527+Akshay-Panwar-01@users.noreply.github.com>
2023-10-16 20:26:24 +0530

    Create 155.py

---
+++ 155.py
@@ -0,0 +1,7 @@
+import sympy 
+import math
+
+'''The answer is the area bounded by the curves x+y<1000 , xy<187500 , x>0 , y>0 
+times 4 which can be calculated easily and comes out to be 455989.80412527 thus total value of D is 1823959.2165010
+
+hence answer is : 18239'''

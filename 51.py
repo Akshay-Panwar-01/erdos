9a671edd0fb6c6f310f9f33ec1cb7bd471f8687e
Akshay-Panwar-01 <65433527+Akshay-Panwar-01@users.noreply.github.com>
2023-10-18 23:53:44 +0530

    Create 51.py

---
+++ 51.py
@@ -0,0 +1,10 @@
+import sympy 
+import math
+
+'''However the formula for the same is summation of int(sqrt(2*k-1))-int(sqrt(k)) from k=1 to k=n but it'll still be harder to calculate so 
+on searching online about the problem I found an article discussing the same problem that is
+ https://math.stackexchange.com/questions/4168160/probability-that-the-sum-of-two-integers-in-1-dots-n-equals-a-perfect-squ '''
+
+print(((4/3)*(math.sqrt(2)-1))*(10**8))
+
+'''The answer is : 55228474'''

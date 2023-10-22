a21d0782c372e0b85b665dd555ec0116431e5d47
Akshay-Panwar-01 <65433527+Akshay-Panwar-01@users.noreply.github.com>
2023-10-23 01:00:47 +0530

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,9 @@
+import sympy 
+import math
+
+'''https://math.stackexchange.com/questions/1349889/game-of-probability'''
+
+x=(1+math.sqrt(5))/2
+print(round(x*1000000))
+
+'''The answer is : 1618034a'''

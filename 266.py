972c2875afcc2e862a5bdade0fe0a53a9ebb0870
Akshay-Panwar-01 <65433527+Akshay-Panwar-01@users.noreply.github.com>
2023-11-07 16:36:51 +0530

    Create 266.py

---
+++ 266.py
@@ -0,0 +1,9 @@
+import sympy 
+import math
+'''numbers will be arranged as they are indexed in base 127 and on the xth place 
+the number would be x-1 so'''
+
+a=20212020/(127**4)
+print(a*(10**7))
+
+'''The answer is : 776953'''

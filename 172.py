843e2f5f66f54fae3b21f5695c40c6b3238c2aa9
Akshay-Panwar-01 <65433527+Akshay-Panwar-01@users.noreply.github.com>
2024-01-01 19:43:16 +0530

    Create 172.py

---
+++ 172.py
@@ -0,0 +1,11 @@
+import sympy
+import math
+import numpy as np 
+
+'''here the formula for S(k,x)=15**k so '''
+m=100086841
+a=pow(18,10**18,m-1)
+b=pow(15,a,m)
+print(b)
+
+'''The answer is : 58460839 '''
